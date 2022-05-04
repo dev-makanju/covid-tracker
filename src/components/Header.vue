9ee0c7c10eab5f1fac8918376ef1fa0776c5107e
miracool <makurseme@gmail.com>
2022-05-04 18:25:07 +0100

    feat: added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,12 @@
         <div class="container px-6 py-3 mx-auto md:flex">
             <div class="flex items-center justify-between">
                 <div>
-                    <a class="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Covid19</a>
+                  <a class="text-2xl font-bold text-blue-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-blue-700 dark:hover:text-blue-300" href="#">Covid19</a>
                 </div>
                 
                 <!-- Mobile menu button -->
                 <div class="flex md:hidden">
-                    <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <button @click="toggleNavbar" type="button" class="text-blue-600 dark:text-blue-200 hover:text-blue-600 dark:hover:text-blue-400 focus:outline-none focus:text-blue-600 dark:focus:text-blue-400" aria-label="toggle menu">
                         <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
                             <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                         </svg>
@@ -18,11 +18,11 @@
             </div>
 
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-            <div class="w-full md:flex md:items-center md:justify-between">
+              <div :class="['w-full  md:flex md:items-center md:justify-between', isNavActive?'open':'close']">
                 <div class="flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0">
-                    <a href="#" class="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 md:mx-2">Home</a>
-                    <a href="#" class="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 md:mx-2">About</a>
-                    <a href="#" class="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-gray-200 hover:bg-gray-900 hover:text-gray-100 md:mx-2">Contact</a>
+                    <a href="#" class="px-2 py-1 text-sm font-medium text-blue-700 transition-colors duration-200 transform rounded dark:text-blue-200 hover:bg-blue-900 hover:text-blue-100 md:mx-2">Home</a>
+                    <a href="#" class="px-2 py-1 text-sm font-medium text-blue-700 transition-colors duration-200 transform rounded dark:text-blue-200 hover:bg-blue-900 hover:text-blue-100 md:mx-2">About</a>
+                    <a href="#" class="px-2 py-1 text-sm font-medium text-blue-700 transition-colors duration-200 transform rounded dark:text-blue-200 hover:bg-blue-900 hover:text-blue-100 md:mx-2">Contact</a>
                 </div>
                 
                 <div class="relative">
@@ -31,10 +31,9 @@
                             <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg>
                     </span>
-
-                    <input type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
+                    <input type="text" class="w-full py-2 pl-10 pr-4 text-blue-700 bg-white border rounded-md dark:bg-blue-800 dark:text-blue-300 dark:border-blue-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
                 </div>
-            </div>
+              </div>
         </div>
     </nav>
   </div>
@@ -42,9 +41,30 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'HelloWorld ',
+  data(){
+      return{
+         isNavActive: null,
+      }
+  },
+  methods:{
+      toggleNavbar(){
+          this.isNavActive = !this.isNavActive
+      }
   }
 }
 </script>
+<style lang="css" scoped>
+    @media (max-width: 768px) {
+      .open{
+        display: block;
+      }
+    }
+
+    @media (max-width: 768px){
+      .close{
+          display: none;
+      }
+    }
+
+</style>
